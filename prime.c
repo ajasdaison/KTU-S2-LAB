9cc04deee1478c5f1e1202d907027c2b84f0fbd9
ajasdaison <ajasdaison1991@gmail.com>
2024-03-17 15:39:13 +0530

    prime.c

---
+++ prime.c
@@ -0,0 +1,25 @@
+/*Read a Natural Number and check whether the number is prime or not*/
+#include <stdio.h>
+int main() {
+  int num, i, flag = 0;
+  printf("Enter the Number:\t");
+  scanf("%d", &num);
+
+  if (num == 1) {
+    printf("1 is neither composite nor prime");
+  } else if (num == 2) {
+    printf("2 is a prime number");
+  } else {
+    for (i = 2; i < num; i++) {
+      if (num % i == 0) {
+        flag = 1;
+        break;
+      }
+    }
+    if (flag == 0) {
+      printf("%d is a prime number", num);
+    } else
+      printf("%d is not a prime number", num);
+  }
+  return 0;
+}
