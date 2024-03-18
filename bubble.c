1c6163e6ba913c5edcebeec9641f8f95f9753c24
ajasdaison <ajasdaison1991@gmail.com>
2024-03-18 19:43:38 +0530

    Squashed multiple branches p9 p10

---
+++ bubble.c
@@ -0,0 +1,30 @@
+// Read n integers, store them in an array and sort the elements in the array
+// using Bubble Sort algorithm
+#include <stdio.h>
+int main() {
+  int n;
+  int temp;
+  printf("Enter the number of elements: ");
+  scanf("%d", &n);
+
+  int arr[n];
+  printf("Enter %d integers:\n", n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+  for(int i =0; i <n-1; i++){
+for (int j = 0 ; j < n - i - 1; j++){
+if (arr[j]>arr[j+1])
+
+
+  {temp = arr[j];
+   arr[j]=arr[j+1];
+   arr[j+1]=temp;}
+  }
+  }
+  printf("Sorted list in ascending order:\n");
+
+    for (int i = 0; i < n; i++)
+	         printf("%d\n", arr[i]);
+  return 0;
+}
---
+++ linear.c
@@ -0,0 +1,32 @@
+// Read n integers, store them in an array and search for an element in the
+// array using an algorithm for Linear Search Correct
+#include <stdio.h>
+int main() {
+  int n;
+  printf("Enter the number of integers: ");
+  scanf("%d", &n);
+  int arr[n];
+  printf("Enter the integers:\n"); // enter the numbers by separating them with
+                                   // spaces
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+  int element;
+  printf("Enter the element to search for: ");
+  scanf("%d", &element);
+  int key = 0;
+  for (int i = 0; i < n; i++) {
+    if (arr[i] == element) {
+      key = i + 1;
+      break;
+    }
+  }
+
+  if (key != 0) {
+    printf("Element %d found at index %d.\n", element, key);
+  } else {
+    printf("Element %d not found in the array.\n", element);
+  }
+
+  return 0;
+}
